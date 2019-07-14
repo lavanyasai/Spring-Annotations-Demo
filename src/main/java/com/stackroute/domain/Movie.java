0e2b63eacffe3cafe1a43e614d7db7312d2c95b0
Lavanya Sai Pentela <lavanyasaipentela@gmail.com>
2019-07-14 16:26:55 +0530

    TASK4

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,9 +4,15 @@ package com.stackroute.domain;
 Displays actor information for respective movies.
  */
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     @Autowired
     private Actor actor;
@@ -36,5 +42,20 @@ public class Movie {
 
     public void initMethod() {
         actor = new Actor("ABC", "Female", 12);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory Called");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean Name : " + s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context Set");
     }
 }
